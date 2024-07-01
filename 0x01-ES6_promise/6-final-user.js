9d705fe6d12d5dc5e1944c2a240a5c737c91b4e8
quduss <shuaibqudus33@gmail.com>
2024-07-01 15:10:10 +0100

    Convert error object to a string

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -5,5 +5,5 @@ export default async function handleProfileSignup(firstName, lastName, fileName)
   const p1 = signUpUser(firstName, lastName);
   const p2 = uploadPhoto(fileName);
   return Promise.allSettled([p1, p2])
-    .then((res) => res.map((x) => ({ status: x.status, value: x.status === 'fulfilled' ? x.value : x.reason })));
+    .then((res) => res.map((x) => ({ status: x.status, value: x.status === 'fulfilled' ? x.value : String(x.reason) })));
 }
