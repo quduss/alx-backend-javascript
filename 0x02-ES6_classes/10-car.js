ae148b815b308a9477eb19594416ff84295c85b3
quduss <shuaibqudus33@gmail.com>
2024-07-04 17:26:19 +0100

    Implement a class named Car
    Constructor attributes:
    brand (String)
    motor (String)
    color (String)
    
    Each attribute is stored in an “underscore” attribute version.
    
    A method named cloneCar that returns a new object of the class is
    implemented.

---
+++ 0x02-ES6_classes/10-car.js
@@ -0,0 +1,36 @@
+/* eslint-disable no-underscore-dangle */
+export default class Car {
+  constructor(brand, motor, color) {
+    this.brand = brand;
+    this.motor = motor;
+    this.color = color;
+  }
+
+  set brand(value) {
+    this._brand = value;
+  }
+
+  get brand() {
+    return this._brand;
+  }
+
+  set motor(value) {
+    this._motor = value;
+  }
+
+  get motor() {
+    return this._motor;
+  }
+
+  set color(value) {
+    this._color = value;
+  }
+
+  get color() {
+    return this._color;
+  }
+
+  cloneCar() {
+    return new this.constructor();
+  }
+}
