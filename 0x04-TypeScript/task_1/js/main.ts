6d0739fbef3e54c53caaf4f653891877da1be82c
quduss <shuaibqudus33@gmail.com>
2024-07-11 12:05:59 +0100

    Write an interface Directors that extends Teacher
    It requires an attribute numberOfReports(number).

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,17 +1,20 @@
 interface Teacher {
-    readonly firstName: string;
-    readonly lastName: string;
-    fullTimeEmployee: boolean;
-    yearsOfExperience?: number;
-    location: string;
-    [propName: string]: any; // Index signature to allow any additional properties
-  }
+  readonly firstName: string;
+  readonly lastName: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
+  location: string;
+  [propName: string]: any; // Index signature to allow any additional properties
+}
 
-  const teacher3: Teacher = {
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+const director1: Directors = {
     firstName: 'John',
-    fullTimeEmployee: false,
     lastName: 'Doe',
     location: 'London',
-    contract: false,
+    fullTimeEmployee: true,
+    numberOfReports: 17,
   };
-  console.log(teacher3);
+  console.log(director1);
