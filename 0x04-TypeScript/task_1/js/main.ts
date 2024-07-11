e69226f432cf575bbd56281407a5b80d7e12e722
quduss <shuaibqudus33@gmail.com>
2024-07-11 12:32:24 +0100

    Write an interface printTeacherFunction
    Write a function printTeacher of this interface:
    
    It accepts two arguments firstName and lastName.
    
    It returns the first letter of the firstName and the full lastName
    
    Example: printTeacher("John", "Doe") -> J. Doe

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -11,10 +11,20 @@ interface Directors extends Teacher {
   numberOfReports: number;
 }
 const director1: Directors = {
-    firstName: 'John',
-    lastName: 'Doe',
-    location: 'London',
-    fullTimeEmployee: true,
-    numberOfReports: 17,
-  };
-  console.log(director1);
+  firstName: 'John',
+  lastName: 'Doe',
+  location: 'London',
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+};
+console.log(director1);
+
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+// Implement the printTeacher function
+const printTeacher: printTeacherFunction = (firstName: string, lastName: string): string => {
+  return `${firstName.charAt(0)}. ${lastName}`;
+};
+console.log(printTeacher("John", "Doe")); // Output: J. Doe
