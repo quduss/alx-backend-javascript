ba9cd790c54f5abc2253a945cdc23ff468be1987
quduss <shuaibqudus33@gmail.com>
2024-09-20 11:18:01 +0100

    Do not dynamically import chai

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,14 +1,9 @@
 const sinon = require('sinon');
+const chai = require('chai');
 const Utils = require('./utils');
 const sendPaymentRequestToApi = require('./3-payment');
 
 describe('sendPaymentRequestToApi', function () {
-  let chai;
-  
-  before(async function () {
-    chai = await import('chai');
-  });
-
   it('should use Utils.calculateNumber to calculate the total', function () {
     const { expect } = chai;
     const calculateNumberSpy = sinon.spy(Utils, 'calculateNumber');
