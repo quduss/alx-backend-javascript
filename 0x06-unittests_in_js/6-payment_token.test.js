5b47d19ffbdf5542095b33dae9e3d2bc3480fe87
quduss <shuaibqudus33@gmail.com>
2024-09-20 13:28:22 +0100

    Do not import chai dynamically

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -1,17 +1,14 @@
 const getPaymentTokenFromAPI = require('./6-payment_token');
+const { expect } = require('chai');
 
-import('chai').then((chai) => {
-  const expect = chai.expect;
-
-  describe('getPaymentTokenFromAPI', function () {
-    it('should return a successful response when success is true', function (done) {
-      getPaymentTokenFromAPI(true)
-        .then((response) => {
-          expect(response).to.have.property('data', 'Successful response from the API');
-          done();
-        })
-        .catch((error) => done(error));
-    });
+describe('getPaymentTokenFromAPI', function () {
+  it('should return a successful response when success is true', function (done) {
+    getPaymentTokenFromAPI(true)
+      .then((response) => {
+        expect(response).to.have.property('data', 'Successful response from the API');
+        done();
+      })
+      .catch((error) => done(error));
   });
 });
   
